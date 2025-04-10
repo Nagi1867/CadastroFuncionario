068a41f265ed86c3c79a9d8316acf8382507641a
Gabriel Kiyoto <120345849+Nagi1867@users.noreply.github.com>
2025-04-10 13:27:53 -0300

    delete

--- src/main/java/com/example/CadastroFuncionario/service/FuncionarioService.java
+++ src/main/java/com/example/CadastroFuncionario/service/FuncionarioService.java
@@ -25,4 +25,8 @@ public class FuncionarioService {
     public Funcionarios insert(Funcionarios obj) {
         return repository.save(obj);
     }
+
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
 }
